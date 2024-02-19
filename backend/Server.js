bcd2dc89ad9250344e61297c4badbea807903b48
Jeremy Davis <32678613+jeremy0919@users.noreply.github.com>
2024-02-18 18:35:39 -0800

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 // Routes
-app.get('/accounts', (req, res) => {
+app.get('/GetUser', (req, res) => {
   // Read account data from JSON file
-  fs.readFile('accounts.json', 'utf8', (err, data) => {
+  fs.readFile('Users.json', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       res.status(500).send('Error reading accounts data');
@@ -20,10 +20,10 @@ app.get('/accounts', (req, res) => {
   });
 });
 
-app.post('/accounts', (req, res) => {
+app.post('/NewUser', (req, res) => {
   // Append new account to JSON file
   const newAccount = req.body;
-  fs.readFile('accounts.json', 'utf8', (err, data) => {
+  fs.readFile('Users.json', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       res.status(500).send('Error reading accounts data');
@@ -31,7 +31,7 @@ app.post('/accounts', (req, res) => {
     }
     const accounts = JSON.parse(data);
     accounts.push(newAccount);
-    fs.writeFile('accounts.json', JSON.stringify(accounts), (err) => {
+    fs.writeFile('Users.json', JSON.stringify(accounts), (err) => {
       if (err) {
         console.error(err);
         res.status(500).send('Error writing accounts data');
@@ -39,6 +39,39 @@ app.post('/accounts', (req, res) => {
       }
       res.status(201).send('Account added successfully');
     });
+  });
+});
+
+app.post('/NewImage', (req, res) => {
+  // Append new account to JSON file
+  const newImage = req.body;
+  fs.readFile('Images.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error reading accounts data');
+      return;
+    }
+    const accounts = JSON.parse(data);
+    accounts.push(newImage);
+    fs.writeFile('Images.json', JSON.stringify(accounts), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error writing accounts data');
+        return;
+      }
+      res.status(201).send('Account added successfully');
+    });
+  });
+});
+app.get('/GetImage', (req, res) => {
+  // Read account data from JSON file
+  fs.readFile('Images.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error reading accounts data');
+      return;
+    }
+    res.json(JSON.parse(data));
   });
 });
 
